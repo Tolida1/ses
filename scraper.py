5eeef008a56b28ac2c30ae5a914b1d352b71d3b7
Tolida1 <105459096+Tolida1@users.noreply.github.com>
2026-07-21 21:21:14 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,8 @@ USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
 # M3U içine yazılacak referer (Boss Sports)
 REFERER = "https://bosssports1019.com/"
 
-OUTPUT_FILE = "boos.m3u"
+# Çıktı dosyası
+OUTPUT_FILE = "boss.m3u"
 
 
 def write_m3u(entries):
